f6167bec0672daae6da671df564e65f618070f14
tiago-pimenta <tiagopimenta@gmail.com>
2025-03-24 23:22:25 -0300

    commit

--- src/main/java/br/com/tiagopimenta/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/tiagopimenta/spring/data/SpringDataApplication.java
@@ -1,20 +1,33 @@
 package br.com.tiagopimenta.spring.data;
 
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.tiagopimenta.spring.data.orm.Cargo;
 import br.com.tiagopimenta.spring.data.repository.CargoRepository;
+import br.com.tiagopimenta.spring.data.service.CrudCargoService;
+import br.com.tiagopimenta.spring.data.service.CrudCargoService;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
-	private final CargoRepository repository;
+	//private final CargoRepository repository;
+	private final CrudCargoService cargoService;
 	
-	public SpringDataApplication(CargoRepository repository) {
+	private Boolean system = true;
+	
+//	public SpringDataApplication(CargoRepository repository) {
+//		
+//		this.repository = repository;
+//		
+//	}
+	
+	public SpringDataApplication(CrudCargoService cargoService) {
 		
-		this.repository = repository;
+		this.cargoService = cargoService;
 		
 	}
 	
@@ -25,10 +38,30 @@ public class SpringDataApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 				
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("Desenvolvedor de Software");
+		Scanner scanner = new Scanner(System.in);
 		
-		repository.save(cargo);
+		while(system) {
+			System.out.println("Qual ação que você quer executar ?");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+			
+			int action = scanner.nextInt();
+			
+			if (action == 1) {
+				
+				cargoService.inicial(scanner);
+				
+			} else {
+				
+				system = false;
+				
+			}
+		}
+		
+//		Cargo cargo = new Cargo();
+//		cargo.setDescricao("Desenvolvedor de Software");
+//		
+//		repository.save(cargo);
 		
 	}
 
